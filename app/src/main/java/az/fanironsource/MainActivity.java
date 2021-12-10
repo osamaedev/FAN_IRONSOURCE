54e866148c51537d47e0af3f988db63bac1fb5e5
Osama <oussama2ste@gmail.com>
2021-12-10 17:29:00 +0100

    :hammer: Ads implementation

--- app/src/main/java/az/fanironsource/MainActivity.java
+++ app/src/main/java/az/fanironsource/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -12,6 +13,8 @@ import com.ironsource.mediationsdk.ISBannerSize;
 import com.ironsource.mediationsdk.IronSource;
 import com.ironsource.mediationsdk.IronSourceBannerLayout;
 import com.ironsource.mediationsdk.logger.IronSourceError;
+import com.ironsource.mediationsdk.model.InterstitialPlacement;
+import com.ironsource.mediationsdk.sdk.BannerListener;
 import com.ironsource.mediationsdk.sdk.InterstitialListener;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,65 +30,114 @@ public class MainActivity extends AppCompatActivity {
 
         AudienceNetworkAds.initialize(this);
         IronSource.init(this, "126cbf735");
-        IronSource.setMetaData("Facebook_IS_CacheFlag" , "IMAGE");
+        IronSource.setMetaData("Facebook_IS_CacheFlag", "IMAGE");
         final FrameLayout bannerContainer = findViewById(R.id.bannerContainer);
-        IronSourceBannerLayout bannerLayout = IronSource.createBanner(this, ISBannerSize.BANNER);
+        bannerLayout = IronSource.createBanner(this, ISBannerSize.BANNER);
 
         FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                 FrameLayout.LayoutParams.WRAP_CONTENT);
         bannerContainer.addView(bannerLayout, 0, layoutParams);
+        bannerLayout.setBannerListener(new BannerListener() {
+            @Override
+            public void onBannerAdLoaded() {
+                Log.d("TAG", "onBannerAdLoaded: Hna banner tloadat");
+                bannerLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onBannerAdLoadFailed(IronSourceError ironSourceError) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        bannerContainer.removeAllViews();
+                    }
+                });
+                Log.d("TAG", "onBannerAdLoadFailed: Code mzn walkin banner ma tloadatch");
+            }
+
+            @Override
+            public void onBannerAdClicked() {
+
+            }
+
+            @Override
+            public void onBannerAdScreenPresented() {
+
+            }
+
+            @Override
+            public void onBannerAdScreenDismissed() {
+
+            }
+
+            @Override
+            public void onBannerAdLeftApplication() {
+
+            }
+        });
         IronSource.loadBanner(bannerLayout, (String) "DefaultBanner");
 
 
         // ------------- Banner IronSource ------------- //
 
-       IronSource.loadInterstitial();
-       IronSource.setInterstitialListener(new InterstitialListener() {
-           @Override
-           public void onInterstitialAdReady() {
+        IronSource.loadInterstitial();
+        IronSource.setInterstitialListener(new InterstitialListener() {
+            @Override
+            public void onInterstitialAdReady() {
+                if (IronSource.isInterstitialPlacementCapped("Hna smit inter f dashboard") && IronSource.isInterstitialReady()) {
+                    IronSource.showInterstitial("Hna smint inter");
+                }
 
+                // Ola t9dar dir hadi bla smit inter nichan lik can m loadi iban
+//                if (IronSource.isInterstitialReady()) {
+//                    IronSource.showInterstitial();
+//                }
+            }
 
-           }
+            @Override
+            public void onInterstitialAdLoadFailed(IronSourceError ironSourceError) {
 
-           @Override
-           public void onInterstitialAdLoadFailed(IronSourceError ironSourceError) {
+            }
 
-           }
+            @Override
+            public void onInterstitialAdOpened() {
 
-           @Override
-           public void onInterstitialAdOpened() {
+            }
 
-           }
+            @Override
+            public void onInterstitialAdClosed() {
 
-           @Override
-           public void onInterstitialAdClosed() {
+            }
 
-           }
+            @Override
+            public void onInterstitialAdShowSucceeded() {
 
-           @Override
-           public void onInterstitialAdShowSucceeded() {
+            }
 
-           }
+            @Override
+            public void onInterstitialAdShowFailed(IronSourceError ironSourceError) {
 
-           @Override
-           public void onInterstitialAdShowFailed(IronSourceError ironSourceError) {
+            }
 
-           }
+            @Override
+            public void onInterstitialAdClicked() {
 
-           @Override
-           public void onInterstitialAdClicked() {
-
-           }
-       });
+            }
+        });
 
 
     }
 
     public void onClick(View view) {
-        IronSource.destroyBanner(bannerLayout);
         Intent intent = new Intent(MainActivity.this, Page_1.class);
         startActivity(intent);
         IronSource.showInterstitial("DefaultInterstitial");
+    }
 
+
+    @Override
+    protected void onDestroy() {
+        IronSource.destroyBanner(bannerLayout);
+        super.onDestroy();
     }
 }
